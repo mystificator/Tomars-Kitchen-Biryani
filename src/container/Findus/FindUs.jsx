f3c8d32e78b5ad707f0b3b6093dbdd5936b7d84a
Vipul Tandon <vipultandon.business@gmail.com>
2022-12-25 02:19:26 +0530

    Update FindUs.jsx

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -13,7 +13,7 @@ const FindUs = () => (
       <div className="app__wrapper-content">
         <p className="p__opensans">Tomar Niwas, Manjupath, Dixitpura Road, Jabalpur, madhya Pradesh 482002</p>
         <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
-        <p className="p__opensans">Mon - Sun: 11:00 am - 11:59 pm</p>
+        <p className="p__opensans">Mon - Sun: 01:00 pm - 11:00 pm</p>
         <p className="p__opensans">On Holidays: Timings may differ</p>
       </div>
       <div className="app__footer-links_icons">
